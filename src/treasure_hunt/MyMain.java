c2c6bff7dcfd4c4a997bcd61914e558a88759499
mohamidi <58640912+mohamidi@users.noreply.github.com>
2019-12-23 02:07:58 -0500

    Update MyMain.java

--- src/treasure_hunt/MyMain.java
+++ src/treasure_hunt/MyMain.java
@@ -3,26 +3,17 @@ import gameNet.*;
 import treasure_hunt.MyGame;
 import treasure_hunt.MyUserInterface;
 
-
-
-// ***************************************************
-public class MyMain extends GameCreator{   
-
+public class MyMain extends GameCreator
+{   
 	public GameNet_CoreGame createGame()
 	{
 		return new MyGame();
 	}
 
-
 	public static void main(String[] args) 
 	{   
 		MyMain myMain = new MyMain(); 
-
-		
-		GameNet_UserInterface  myUserInterface = 
-    	  		new MyUserInterface();
-		 
-
+		GameNet_UserInterface  myUserInterface = new MyUserInterface();
 		myMain.enterGame(myUserInterface); 
 	}
 }
